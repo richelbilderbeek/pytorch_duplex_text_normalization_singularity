03b6647f1340bf73b11317cc72ed48a34a36b80a
richelbilderbeek <richel@richelbilderbeek.nl>
2023-04-23 09:19:16 +0200

    Fix bash style

--- run_singularity_container.sh
+++ run_singularity_container.sh
@@ -10,8 +10,6 @@
 singularity run --cleanenv --bind "$PWD"/scripts/ pytorch.sif \
   python3 /opt/NeMo/examples/nlp/duplex_text_normalization/duplex_text_normalization_train.py \
   data.validation_ds.data_path=validation_10000.txt \
-  data.train_ds.data_path=train_10000.txt \       
+  data.train_ds.data_path=train_10000.txt \
   mode={tn,itn,joint} \
   lang={en,ru,de}
-
-
