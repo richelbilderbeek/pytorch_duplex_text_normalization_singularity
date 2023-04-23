ed78f851c963a14d998c82288d7bda0c3d161317
richelbilderbeek <richel@richelbilderbeek.nl>
2023-04-23 11:19:10 +0200

    Can clone example

--- clone_example.sh
+++ clone_example.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 git clone https://github.com/NVIDIA/NeMo --depth 1
-wget https://raw.githubusercontent.com/richelbilderbeek/pytorch_duplex_text_normalization_singularity/master/validation_10000.txt /opt/NeMo/examples/nlp/duplex_text_normalization
-wget https://raw.githubusercontent.com/richelbilderbeek/pytorch_duplex_text_normalization_singularity/master/train_10000.txt /opt/NeMo/examples/nlp/duplex_text_normalization
+wget https://raw.githubusercontent.com/richelbilderbeek/pytorch_duplex_text_normalization_singularity/master/validation_10000.txt --output-document ./NeMo/examples/nlp/duplex_text_normalization/validation_10000.txt --no-clobber
+wget https://raw.githubusercontent.com/richelbilderbeek/pytorch_duplex_text_normalization_singularity/master/train_10000.txt --output-document ./NeMo/examples/nlp/duplex_text_normalization/train_10000.txt --no-clobber
